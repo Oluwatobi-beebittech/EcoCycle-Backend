54ea40b07b367831c866d6771855956e808315eb
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-11-16 23:40:35 +0100

    chore: updated users schema and route versioning

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,66 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+enum EcoChampion {
+  ECO_PROCESSOR = 'ECO_PROCESSOR',
+  ECO_COLLATOR = 'ECO_COLLATOR',
+}
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Generated('uuid')
+  userId: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+  })
+  firstName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+  })
+  lastName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    unique: true,
+  })
+  phoneNumber: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: EcoChampion,
+    default: EcoChampion.ECO_COLLATOR,
+  })
+  ecoChampion: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
