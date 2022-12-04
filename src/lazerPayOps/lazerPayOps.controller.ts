cf68f57bc8b367637ea09859717f9f51f0e2c919
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-12-04 14:12:24 +0100

    docs: updated readme with lazerpay third-party integration copy

--- src/lazerPayOps/lazerPayOps.controller.ts
+++ src/lazerPayOps/lazerPayOps.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
